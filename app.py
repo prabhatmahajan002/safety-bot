8a3c33124cb0ab18034ac0466fd8526f9c4254b5
prabhatmahajan002 <prabhatmahajan45@gmail.com>
2026-04-30 11:26:18 +0530

    Fixed app.run syntax

--- app.py
+++ app.py
@@ -424,3 +424,4 @@ def home():
 # ─── Run ──────────────────────────────────────────────────────────────────────
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
